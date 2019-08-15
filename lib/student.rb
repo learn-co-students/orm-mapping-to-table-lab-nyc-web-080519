a579ddea1b667e8655884280f0eb0c56ca376e82
github username <github email address>
2019-08-14 20:57:51 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,24 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+
+
+    def self.create_table
+        SQL = <<-SQL
+        CREATE TABLE IF NOT EXISTS (id = nil)
+    end
+
+    def self.drop_table
+
+    end
+
+    def save
+
+    end
+
+    def self.create(name:, grade:)
+
+    end
+
   
 end
